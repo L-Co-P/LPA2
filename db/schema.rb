23fb8c80bc5dd116233c993794afef54b69d545e
Loann Peurey <lpeurey@utbm.fr>
2019-03-09 11:39:21 +0100

    scaffold produits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190309081330) do
+ActiveRecord::Schema.define(version: 20190309102328) do
+
+  create_table "produits", force: :cascade do |t|
+    t.string "nom"
+    t.string "description"
+    t.float "prix"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
